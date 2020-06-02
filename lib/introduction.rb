18e0b31e0f72fe3e2ecd075abe34c4c0a1e2e551
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-02 20:38:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,12 +15,12 @@ introduction ("Tigist")
 
 def introduction_with_language(name,language)
   
-  puts " Hi, my name is #{name}" and I am learning to program in #{language}."
+  puts " Hi, my name is #{name} and I am learning to program in #{language}."
   
 end
 
 
-introduction_with_language ("Tigist" "Ruby")
+introduction_with_language ( "Tigist" "Ruby" )
 
 
 
